ffbf88d7634e02a68855ffaeb5ffa12cb075f2ad
joaoantoniosiqueira <joao.calvet.siqueira@gmail.com>
2023-11-18 11:00:46 -0300

    initial datasource configuration

---
+++ src/main/java/br/com/amplitude/amplitudefood/amplitudefood/AmplitudefoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.amplitude.amplitudefood.amplitudefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplitudefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmplitudefoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amplitude/amplitudefood/amplitudefood/AmplitudefoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amplitude.amplitudefood.amplitudefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplitudefoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
